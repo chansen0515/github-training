0b44b2e4c9c9f276bd98c472fe5420daf8ad11a0
hunyo <june.pavia@gmail.com>
2022-08-23 23:42:29 +0800

    add new object

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,9 @@ const firstName:string = "David Jones";
 console.log("firstName") 
 console.log("blahblah", 'Blahhhhh');
 
+let info: object = {
+    age: 10,
+    address: "manila"
+}
+
+console.log(info, 'INFO');
