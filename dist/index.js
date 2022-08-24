4a6f0ab83f792c87d248ad0cb76f4b9119db9d4f
Pirc Costillas <bennington12323@gmail.com>
2022-08-24 17:38:09 +0800

    save index.js

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,11 @@
 "use strict";
 let names = "Chuvash Pro Dota 2 player and he won 5 TIs";
-let age = 34;
+let age = 35;
 console.log(names);
 const firstName = "David Jones";
 console.log("firstName");
-console.log("blahblah", "Blahhhhh");
+console.log("blahblah", 'Blahhhhh');
 let info = {
-  age: 20,
-  address: "manila",
+    age: 20,
+    address: 'manila',
 };
